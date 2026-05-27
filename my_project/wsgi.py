a6ae8959260d9277b6ffda63542b8c40bdbcf5f3
tera <spaunsik1992@outlook.comgit config --global user.email spaunsik1992@outlook.com>
2026-05-27 18:20:09 +0200

    make admin access and initial content deploy-safe

--- my_project/wsgi.py
+++ my_project/wsgi.py
@@ -24,3 +24,49 @@ if os.environ.get('AUTO_MIGRATE_ON_STARTUP', '1') == '1':
         call_command('migrate', interactive=False)
     except Exception:
         pass
+
+    try:
+        from django.contrib.auth import get_user_model
+        from main.models import Module, Topic, Presentation, TopicImage
+        from original_views import MODULES
+
+        User = get_user_model()
+        admin_username = os.environ.get('ADMIN_USERNAME', 'admin')
+        admin_password = os.environ.get('ADMIN_PASSWORD', 'Admin1234')
+        admin_email = os.environ.get('ADMIN_EMAIL', 'admin@example.com')
+
+        if not User.objects.filter(username=admin_username, is_superuser=True).exists():
+            User.objects.create_superuser(admin_username, admin_email, admin_password)
+
+        if Module.objects.count() == 0:
+            for module_data in MODULES:
+                module = Module.objects.create(
+                    title=module_data['title'],
+                    description=module_data.get('description', ''),
+                    order=module_data.get('id', 0),
+                    info=module_data.get('info', ''),
+                )
+                for topic_data in module_data.get('topics', []):
+                    topic = Topic.objects.create(
+                        module=module,
+                        title=topic_data['title'],
+                        description=topic_data.get('description', ''),
+                        order=topic_data.get('id', 0),
+                        info=topic_data.get('info', ''),
+                    )
+                    for pres_data in topic_data.get('presentations', []):
+                        Presentation.objects.create(
+                            topic=topic,
+                            title=pres_data.get('title', ''),
+                            file=None,
+                            order=0,
+                        )
+                    for img_data in topic_data.get('images', []):
+                        TopicImage.objects.create(
+                            topic=topic,
+                            caption=img_data.get('caption', ''),
+                            image=None,
+                            order=0,
+                        )
+    except Exception:
+        pass
